98b36d1f9b2060b6059610445bf4a8eb63144460
Will Olson <will.olson11@gmail.com>
2020-06-03 23:38:56 -0700

    Configure rails to work with ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << ENV['NGROK_HOST'] if ENV['NGROK_HOST'].present?
 end
