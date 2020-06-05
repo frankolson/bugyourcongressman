e3e02a55dd79732bc42b368112ad3a997a447735
Will Olson <will.olson11@gmail.com>
2020-06-04 17:38:44 -0700

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post 'switchboards/representatives', to: 'switchboards#representatives'
   post 'switchboards/no_zipcode', to: 'switchboards#no_zipcode'
   post 'switchboards/dial', to: 'switchboards#dial'
+
+  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
