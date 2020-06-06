c09d45a8c7012609f23093fa7662ad59ad5d319e
Will Olson <will.olson11@gmail.com>
2020-06-06 16:26:35 -0700

    Convert phone tree to text to use i18n

--- app/controllers/switchboards_controller.rb
+++ app/controllers/switchboards_controller.rb
@@ -5,11 +5,11 @@ class SwitchboardsController < ApplicationController
     response.gather(num_digits: '5', action: switchboards_representatives_path, timeout: 20) do |gather|
       alice_says(
         requester: gather,
-        message: "Thanks for using Bug Your Congressman dot com.",
+        message: t('switchboard.welcome.intro'),
       )
       alice_says(
         requester: gather,
-        message: "Please enter your 5 digit zipcode to get a list of representatives you can call.",
+        message: t('switchboard.welcome.prompt'),
       )
     end
 
@@ -27,7 +27,11 @@ class SwitchboardsController < ApplicationController
         congressmen.each_with_index do |congressman, index|
           alice_says(
             requester: response,
-            message: "Press #{index+1} for #{congressman.name}.",
+            message: t(
+              'switchboard.representatives.prompt',
+              digit: index + 1,
+              name: congressman.name
+            ),
           )
         end
       end
@@ -47,7 +51,7 @@ class SwitchboardsController < ApplicationController
     response = Twilio::TwiML::VoiceResponse.new
     alice_says(
       requester: response,
-      message: "Please wait while we connect you to #{member.name}.",
+      message: t('switchboard.dial.instructions', name: member.name),
     )
     response.dial(number: member.phones.first)
     response.hangup
@@ -60,7 +64,7 @@ class SwitchboardsController < ApplicationController
     response = Twilio::TwiML::VoiceResponse.new
     alice_says(
       requester: response,
-      message: "We couldn't detect your zipcode, so we're connecting you to the main Congress switchboard in Washington, DC.",
+      message: t('switchboard.no_zipcode.description'),
     )
     response.dial(number: '2022243121')
     response.hangup
---
+++ test/controllers/switchboard_controller_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class MockRepresentative
+  def name
+    'Jacky Rosen'
+  end
+
+  def phones
+    ['202-224-6244']
+  end
+end
+
+class SwitchboardControllerTest < ActionDispatch::IntegrationTest
+  test "should post welcome" do
+    post switchboards_welcome_url
+
+    assert_response :success
+    assert_select 'Say', I18n.t('switchboard.welcome.intro')
+    assert_select 'Say', I18n.t('switchboard.welcome.prompt')
+  end
+
+  test "should post representatives with results" do
+    CivicInformation::Representative.stubs(:where).
+      returns([MockRepresentative.new])
+
+    post switchboards_representatives_url
+
+    assert_response :success
+    assert_select 'Say', count: 1, text: I18n.t(
+        'switchboard.representatives.prompt',
+        digit: 1,
+        name: 'Jacky Rosen'
+      )
+  end
+
+  test "should post representatives with no results" do
+    CivicInformation::Representative.stubs(:where).returns([])
+
+    post switchboards_representatives_url
+
+    assert_response :success
+    assert_select 'Say', count: 0
+    assert_select 'Redirect', switchboards_no_zipcode_path
+  end
+
+  test "should post dial" do
+    CivicInformation::Representative.stubs(:where).
+      returns([MockRepresentative.new])
+
+    post switchboards_dial_url(Digits: '1')
+
+    assert_response :success
+    assert_select 'Say', I18n.t(
+        'switchboard.dial.instructions',
+        name: 'Jacky Rosen'
+      )
+    assert_select 'Dial', '202-224-6244'
+  end
+
+  test "should post no_zipcode" do
+    post switchboards_no_zipcode_url
+
+    assert_response :success
+    assert_select 'Say', I18n.t('switchboard.no_zipcode.description')
+  end
+end
