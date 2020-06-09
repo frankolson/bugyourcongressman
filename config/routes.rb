c8cf45d28a3685dc9b14dd15ffb28c668bed78b0
Will Olson <will.olson11@gmail.com>
2020-06-08 19:52:42 -0700

    Use i18n for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   post 'switchboards/no_zipcode', to: 'switchboards#no_zipcode'
   post 'switchboards/dial', to: 'switchboards#dial'
 
-  get 'pages/about'
-  root 'pages#home'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'pages/about'
+    root 'pages#home'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
