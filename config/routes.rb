76784082db987513c47463bb7299ec589b8da790
Will Olson <will.olson11@gmail.com>
2020-06-10 18:59:22 -0700

    Add about congres page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   post 'switchboards/enter_zipcode', to: 'switchboards#enter_zipcode'
 
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root 'pages#home'
+
+    get 'about-qualified-immunity', to: 'pages#about', as: :about_qualified_immunity
+    get 'about-congress', to: 'pages#congress', as: :about_congress
+
     post 'switchboards/representatives', to: 'switchboards#representatives'
     post 'switchboards/no_zipcode', to: 'switchboards#no_zipcode'
     post 'switchboards/dial', to: 'switchboards#dial'
 
-    get 'pages/about'
-    root 'pages#home'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
